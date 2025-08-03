107c36f901dc956059c111509d3f372bfe113f6c
luqin2007 <lq2007lq@hotmail.com>
2025-08-03 12:37:55 +0800

    删除 utils

--- src/components/bookmarks/item.jsx
+++ src/components/bookmarks/item.jsx
@@ -2,7 +2,6 @@ import classNames from "classnames";
 import ResolvedIcon from "components/resolvedicon";
 import { useContext } from "react";
 import { SettingsContext } from "utils/contexts/settings";
-import { getHost } from "utils/utils";
 
 export default function Item({ bookmark, iconOnly = false }) {
   const description = bookmark.description ?? new URL(bookmark.href).hostname;
@@ -61,4 +60,14 @@ export default function Item({ bookmark, iconOnly = false }) {
       </a>
     </li>
   );
+}
+
+function getHost(url) {
+  if (url.includes('//')) {
+      url = url.substring(url.indexOf('//') + 2);
+  }
+  if (url.includes('/')) {
+      url = url.substring(0, url.indexOf('/'));
+  }
+  return url;
 }
--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function getHost(url) {
-    if (url.includes('//')) {
-      url = url.substring(url.indexOf('//') + 2);
-    }
-    if (url.includes('/')) {
-      url = url.substring(0, url.indexOf('/'));
-    }
-    return url;
-  }
