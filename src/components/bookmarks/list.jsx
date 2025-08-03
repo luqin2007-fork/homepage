3c0924859b1ae08d3c720cb22fc21e0deb1d6df9
luqin2007 <lq2007lq@hotmail.com>
2025-08-03 12:33:53 +0800

    子项过多时显示两列

--- src/components/bookmarks/list.jsx
+++ src/components/bookmarks/list.jsx
@@ -5,6 +5,8 @@ import { columnMap } from "../../utils/layout/columns";
 
 export default function List({ bookmarks, layout, bookmarksStyle }) {
   let classes = layout?.style === "row" ? `grid ${columnMap[layout?.columns]} gap-x-2` : "flex flex-col bookmark-list";
+  // 子项过多时显示两列
+  if (bookmarks.length >= 5) classes = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-2 mb-2 mt-3 bookmark-list' 
   const style = {};
   if (layout?.iconsOnly || bookmarksStyle === "icons") {
     classes = "grid gap-2 bookmark-list";
